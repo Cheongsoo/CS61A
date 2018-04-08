800096d4487da84d88083add07e9be5aa76158b7
Cheongsoo <Cheongsoo@users.noreply.github.com>
2018-04-08 13:42:53 -0700

    Add files via upload
    Project 3

--- utils.py
+++ utils.py
@@ -1,88 +1,81 @@
-"""Utilities for Maps"""
+def class_method_wrapper(method, pre=None, post=None):
+    """Given a class METHOD and two wrapper function, a PRE-function and
+    POST-function, first calls the pre-wrapper, calls the wrapped class method,
+    and then calls the post-wrapper.
+    
+    All wrappers should have the parameters (self, rv, *args). However,
+    pre-wrappers will always have `None` passed in as `rv`, since a return
+    value has not been evaluated yet.
 
-from math import sqrt
-from random import sample
-
-# Rename the built-in zip (http://docs.python.org/3/library/functions.html#zip)
-_zip = zip
-
-def map_and_filter(s, map_fn, filter_fn):
-    """Returns a new list containing the results of calling map_fn on each
-    element of sequence s for which filter_fn returns a true value.
-
-    >>> square = lambda x: x * x
-    >>> is_odd = lambda x: x % 2 == 1
-    >>> map_and_filter([1, 2, 3, 4, 5], square, is_odd)
-    [1, 9, 25]
+    >>> def pre_wrapper(instance, rv, *args):
+    ...     print('Pre-wrapper called: {0}'.format(args))
+    >>> def post_wrapper(instance, rv, *args):
+    ...     print('Post-wrapper called: {0} -> {1}'.format(args, rv))
+    >>> class Foo(object):
+    ...     def __init__(self):
+    ...         self.bar = 20
+    ...     def method(self, var1, var2):
+    ...         print('Original method called')
+    ...         return var1 + var2 + self.bar
+    >>> Foo.method = class_method_wrapper(Foo.method, pre_wrapper, post_wrapper)
+    >>> f = Foo()
+    >>> x = f.method(1, 2)
+    Pre-wrapper called: (1, 2)
+    Original method called
+    Post-wrapper called: (1, 2) -> 23
+    >>> x
+    23
     """
-    # BEGIN Question 0
-   
-    return [map_fn(element) for element in s if filter_fn(element)]
-    # END Question 0
+    def wrapped_method(self, *args):
+        pre(self, None, *args) if pre else None
+        rv = method(self, *args)
+        post(self, rv, *args) if post else None
+        return rv
+    return wrapped_method
 
-def key_of_min_value(d):
-    """Returns the key in a dict d that corresponds to the minimum value of d.
-
-    >>> letters = {'a': 6, 'b': 5, 'c': 4, 'd': 5}
-    >>> min(letters)
-    'a'
-    >>> key_of_min_value(letters)
-    'c'
+def print_expired_insects(self, rv, *args):
+    """Post-wrapper for Insect.reduce_armor, and will print a message if the
+    insect has expired (armor reduced to 0).
+    
+    >>> from ants import Insect, Bee, ThrowerAnt, Place
+    >>> Insect.reduce_armor = class_method_wrapper(Insect.reduce_armor,
+    ...         pre=print_expired_insects)
+    >>> place = Place('Test')
+    >>> bee = Bee(3)
+    >>> place.add_insect(bee)
+    >>> bee.reduce_armor(2)
+    >>> bee.reduce_armor(1)
+    Bee(Test) ran out of armor and expired
+    >>> thrower = ThrowerAnt()
+    >>> place.add_insect(thrower)
+    >>> thrower.reduce_armor(1)
+    ThrowerAnt(Test) ran out of armor and expired
     """
-    # BEGIN Question 0
-   
-    return min(d, key=lambda element:d[element])
-    # END Question 0
+    if self.armor <= args[0]:
+        print('{0}({1}) ran out of armor and expired'.format(
+            type(self).__name__, self.place))
 
-def zip(*sequences):
-    """Returns a list of lists, where the i-th list contains the i-th
-    element from each of the argument sequences.
+def print_thrower_target(self, rv, *args):
+    """Prints the target of a ThrowerAnt, if the ThrowerAnt found a target.
 
-    >>> zip(range(0, 3), range(3, 6))
-    [[0, 3], [1, 4], [2, 5]]
-    >>> for a, b in zip([1, 2, 3], [4, 5, 6]):
-    ...     print(a, b)
-    1 4
-    2 5
-    3 6
-    >>> for triple in zip(['a', 'b', 'c'], [1, 2, 3], ['do', 're', 'mi']):
-    ...     print(triple)
-    ['a', 1, 'do']
-    ['b', 2, 're']
-    ['c', 3, 'mi']
+    >>> from ants import *
+    >>> hive = Hive(AssaultPlan())
+    >>> dimensions = (1, 9)
+    >>> colony = AntColony(None, hive, ant_types(), dry_layout, dimensions)
+    >>> ThrowerAnt.nearest_bee = class_method_wrapper(ThrowerAnt.nearest_bee,
+    ...         post=print_thrower_target)
+    >>> thrower = ThrowerAnt()
+    >>> short = ShortThrower()
+    >>> bee = Bee(5)
+    >>> colony.places['tunnel_0_1'].add_insect(short)
+    >>> colony.places['tunnel_0_0'].add_insect(thrower)
+    >>> colony.places['tunnel_0_5'].add_insect(bee)
+    >>> thrower.action(colony)
+    ThrowerAnt(1, tunnel_0_0) targeted Bee(5, tunnel_0_5)
+    >>> short.action(colony)    # Bee not in range of ShortThrower
+    >>> bee.action(colony)      # Bee moves into range
+    >>> short.action(colony)
+    ShortThrower(1, tunnel_0_1) targeted Bee(4, tunnel_0_4)
     """
-    return list(map(list, _zip(*sequences)))
-
-def enumerate(s, start=0):
-    """Returns a list of lists, where the i-th list contains i+start and
-    the i-th element of s.
-
-    >>> enumerate([6, 1, 'a'])
-    [[0, 6], [1, 1], [2, 'a']]
-    >>> enumerate('five', 5)
-    [[5, 'f'], [6, 'i'], [7, 'v'], [8, 'e']]
-    """
-    # BEGIN Question 0
-    return zip(range(start,start+len(s)), s)
-    # END Question 0
-
-def distance(pos1, pos2):
-    """Returns the Euclidean distance between pos1 and pos2, which are pairs.
-
-    >>> distance([1, 2], [4, 6])
-    5.0
-    """
-    return sqrt((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2)
-
-def mean(s):
-    """Returns the arithmetic mean of a sequence of numbers s.
-
-    >>> mean([-1, 3])
-    1.0
-    >>> mean([0, -3, 2, -1])
-    -0.5
-    """
-    # BEGIN Question 1
-    assert len(s) != 0, "empty sequences are not allowd!"
-    return (sum(s) / len(s))
-    # END Question 1
+    if rv is not None:
+        print('{0} targeted {1}'.format(self, rv))
